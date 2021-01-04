2970052e97d2a4d12c1b903b1dfdc52076c7ecb9
Hieunv <hieunv2406@gmail.com>
2021-01-04 22:16:59 +0700

    hieunv create new project

---
+++ src/main/java/com/myproject/PetStoresApplication.java
@@ -0,0 +1,34 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class PetStoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetStoresApplication.class, args);
+    }
+
+    @Bean
+    public MessageSource getMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("vi", "VN")); // Set default Locale as US
+        return localeResolver;
+    }
+}
